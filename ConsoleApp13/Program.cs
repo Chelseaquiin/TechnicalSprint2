82ed3e19057bc6c041cda54d508b25450d0ba2b8
Uzoamaka Nweze <uzonweze@gmail.com>
2022-09-13 11:36:45 +0100

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+
+            string sentence = Console.ReadLine();
+
+            string pigLatin = ToPigLatin(sentence);
+
+            Console.WriteLine(pigLatin);
+
+            Console.WriteLine("Enter a pigLatin sentence: ");
+
+            string pigSentence = Console.ReadLine();
+
+            string english = ToEnglish(pigSentence);
+
+            Console.WriteLine(english);
+
+
+        }
+
+        public static string ToPigLatin(string sentence)
+        {
+            List<string> pigWords = new List<string>();
+
+            foreach (string word in sentence.Split(' '))
+            {
+                pigWords.Add(word.Substring(1) + word.ElementAt(0) + "ay");
+            }
+
+            return string.Join(" ", pigWords);
+        }
+
+        public static string ToEnglish(string pigLatin)
+        {
+            List<string> englishWords = new List<string>();
+
+            foreach (string word in pigLatin.Split(' '))
+            {
+
+               string newWord = word.Substring(0, word.Length - 2);
+                englishWords.Add(newWord.ElementAt(newWord.Length - 1) + newWord.Substring(0, newWord.Length - 1));
+
+            }
+            return string.Join(" ", englishWords);
+
+
+        }
+    }
+    }
+
---
+++ Question1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            int year = 2022;
+
+            while (count < 80)
+            {
+                if (year % 4 == 0)
+                {
+                    if (year % 100 == 0 && year % 400 == 0)
+                    {
+
+                    }
+                    else if (year % 100 != 0)
+                    {
+
+                    }
+
+                    Console.WriteLine(year);
+
+                }
+                year++;
+                count++;
+            }
+        }
+    }
+}
